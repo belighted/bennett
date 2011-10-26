7281d6b1df41fe093d0ceb146a4cecde7c24f217
Amant Stephane <stephane.amant@belighted.com>
2011-10-26 16:06:07 +0200

    Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :find_projects
-  
+  before_filter :authenticate
+
   def find_projects
     @projects = Project.all
   end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ci' && password == 'belighted!1234'
+    end
+  end
+
 end
