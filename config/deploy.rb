9e6ac141a0f754c4116f5fd851cd5c87b1075a21
Amant Stephane <stephane.amant@belighted.com>
2011-12-28 13:51:37 +0100

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,15 @@ set :branch,            "master"
 set :user,              "nja"
 set :deploy_to,         "/Users/nja/Sites/beci"
 set :rails_env,         "production"
-set :ssh_options, {:forward_agent => true}
-set :use_sudo, false
+set :ssh_options,       {:forward_agent => true}
+set :use_sudo,          false
 
 
 role :web, "192.168.1.99"                          # Your HTTP server, Apache/etc
 role :app, "192.168.1.99"                          # This may be the same as your `Web` server
 role :db,  "192.168.1.99", :primary => true        # This is where Rails migrations will run
 
+after "deploy:update_code", "deploy:symlink_directories_and_files"
 
 namespace :deploy do
   task :start do ; end
@@ -24,4 +25,10 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  desc 'Symlink shared directories and files'
+  task :symlink_directories_and_files do
+    run "ln -s #{shared_path}/.rvmrc #{release_path}/.rvmrc"
+  end
+
 end
