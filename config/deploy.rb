35249aa8d8675369320f4728d41a6cff960140a1
Amant Stephane <stephane.amant@belighted.com>
2011-12-28 14:00:02 +0100

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require 'capistrano_colors'
+require "bundler/capistrano"
 
 set :application,       "beci"
 set :repository,        "git@github.com:belighted/beci.git"
@@ -17,18 +18,10 @@ role :web, "192.168.1.99"                          # Your HTTP server, Apache/et
 role :app, "192.168.1.99"                          # This may be the same as your `Web` server
 role :db,  "192.168.1.99", :primary => true        # This is where Rails migrations will run
 
-after "deploy:update_code", "deploy:symlink_directories_and_files"
-
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-
-  desc 'Symlink shared directories and files'
-  task :symlink_directories_and_files do
-    run "ln -s #{shared_path}/.rvmrc #{release_path}/.rvmrc"
-  end
-
 end
