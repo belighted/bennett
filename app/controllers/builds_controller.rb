7684495f4c7e105ec47f826e1904a9680e11a091
Joel Cogen <joel@joelcogen.com>
2012-03-22 16:01:46 +0100

    304 when git hook but no change on branch

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -13,11 +13,13 @@ class BuildsController < ApplicationController
         if @build.save
           Resque.enqueue(CommitsFetcher, @build.id)
           format.html { redirect_to @project, notice: 'Build successfully added to queue.' }
-          format.json { render json: @build, status: :created, location: @build }
+          format.json { render json: @build, status: :created }
         else
           format.html { redirect_to @project, notice: 'Error adding build.' }
           format.json { render json: @build.errors, status: :unprocessable_entity }
         end
+      else
+        format.json { render status: :not_modified, json: {} }
       end
     end
   end
