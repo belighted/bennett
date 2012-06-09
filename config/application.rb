b424e653a18b583c7922eae9a7a74942bb9720b3
Joel Cogen <joel@joelcogen.com>
2012-06-09 15:37:52 +0200

    Make Devise and the mailer use the configured hostname. Fixes #2

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Bennett
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
 
     # Enable the asset pipeline
     config.assets.enabled = true
