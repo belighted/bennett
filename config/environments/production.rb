716f241cf0141c562d53e24034f1c2e8213e6e19
zdk <nx2zdk@gmail.com>
2013-10-02 17:01:29 +0700

    Test action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,11 @@ Bennett::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-replay@example.com'}
+
   HOST = ENV['HOSTNAME'] || "ci.belighted.com"
   config.action_mailer.default_url_options = {
     :host => HOST,
