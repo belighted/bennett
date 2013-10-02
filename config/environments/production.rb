853e3a118ba3107966a0d8ba890f93d4649c0783
zdk <nx2zdk@gmail.com>
2013-10-02 18:40:51 +0700

    Remove undefined default_options method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,6 @@ Bennett::Application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-replay@example.com'}
 
   HOST = ENV['HOSTNAME'] || "ci.belighted.com"
   config.action_mailer.default_url_options = {
